5579979c9142edb4469d68ef293a43d25a3287fd
Luis Manyari <lmanyari@hotmail.com>
2023-11-21 23:38:40 -0500

    :P~ First Commit

--- src/App.js
+++ src/App.js
@@ -25,24 +25,38 @@
 // export default App;
 
 
-import React from "react";
-import Fruits from "./Fruits";
-import FruitsCounter from "./FruitsCounter";
+// import React from "react";
+// import Fruits from "./Fruits";
+// import FruitsCounter from "./FruitsCounter";
+
+// function App() {
+//   const [fruits] = React.useState([
+//       {fruitName: 'apple', id: 1},
+//       {fruitName: 'apple', id: 2},
+//       {fruitName: 'plum', id: 3},
+//   ]);
+
+//   return (
+//     <div className="App">
+//       <h1>Where should the state go?</h1>
+//       <Fruits fruits={fruits} />
+//       <FruitsCounter fruits={fruits} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 
 function App() {
-  const [fruits] = React.useState([
-      {fruitName: 'apple', id: 1},
-      {fruitName: 'apple', id: 2},
-      {fruitName: 'plum', id: 3},
-  ]);
-
-  return (
-    <div className="App">
-      <h1>Where should the state go?</h1>
-      <Fruits fruits={fruits} />
-      <FruitsCounter fruits={fruits} />
-    </div>
-  );
+  
+  return ( 
+     <div> 
+<button onClick={ () => console.log('clicked') }> 
+  Click me
+</button>
+     </div>
+  )
 }
-
-export default App;
+export default App
